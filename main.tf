fa49e7d171d2040f37743a8b3e3e43ef95167fad
Arunim Chaudhary <47343112+arunim2405@users.noreply.github.com>
2024-10-11 18:36:26 +0530

    stackguardian-nonprod

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "bucket" {
   version = "~> 6.0"
 
   name       = "arunim-gcp-bucket"
-  project_id = var.project_id
+  project_id = "stackguardian-nonprod"
   location   = "us"
 
   lifecycle_rules = [{
@@ -13,7 +13,7 @@ module "bucket" {
     condition = {
       age            = 365
       with_state     = "ANY"
-      matches_prefix = var.project_id
+      matches_prefix = "stackguardian-nonprod"
     }
   }]
 
